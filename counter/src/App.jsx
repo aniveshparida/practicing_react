e50c9ca235ad697a370695de90f9bb6fd2d93c05
aniveshparida <aniveshparida8@gmail.com>
2026-03-10 22:29:36 +0530

    made an input box for password for eye

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -2,18 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import {Eye,EyeOff} from 'lucide-react'
 function App() {
-  const [count, setCount] = useState(0)
-   const [input,setInput] = useState('')
+  const [password,setPassword]=useState('');
+  const [isVisible,setIsVisible]=useState(false);
   const handleChange=(e)=>{
-    setInput(e.target.value);
-    setCount(input.length);
+    setPassword(e.target.value);
+  }
+  const handleClick=()=>{
+    setIsVisible(!isVisible);
   }
   return (
     <div>
-       <input style={{color: count>100 ? 'red' : 'white'}} type="text" value={input} onChange={handleChange}/>
-       <h1>{count}</h1>
+     <input type={isVisible? 'text':'password'} value={password} onChange={handleChange}></input>
+     <button onClick={handleClick}>{!isVisible?<EyeOff size={20}/>:<Eye size={20}/>}</button>
     </div>
   )
 }
